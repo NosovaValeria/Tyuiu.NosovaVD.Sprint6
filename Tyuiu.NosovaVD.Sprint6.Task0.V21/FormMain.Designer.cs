43b57d76c5f7b8ce2abf04047f1668d74230d4c3
NosovaValeria <Tyuiu.NosovaVD@outlook.com>
2023-12-03 16:26:03 +0500

    Task0

--- Tyuiu.NosovaVD.Sprint6.Task0.V21/FormMain.Designer.cs
+++ Tyuiu.NosovaVD.Sprint6.Task0.V21/FormMain.Designer.cs
@@ -29,8 +29,6 @@ namespace Tyuiu.NosovaVD.Sprint6.Task0.V21
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
-            this.pictureBoxNamedTwo_NVD = new System.Windows.Forms.PictureBox();
             this.groupBoxNamedTwo_NVD = new System.Windows.Forms.GroupBox();
             this.textBoxNamedFour_NVD = new System.Windows.Forms.TextBox();
             this.groupBoxNamedThree_NVD = new System.Windows.Forms.GroupBox();
@@ -41,25 +39,17 @@ namespace Tyuiu.NosovaVD.Sprint6.Task0.V21
             this.groupBoxNameddTwo_NVD = new System.Windows.Forms.GroupBox();
             this.buttonDone_NVD = new System.Windows.Forms.Button();
             this.buttonHelp_NVD = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxNamedTwo_NVD)).BeginInit();
+            this.pictureBoxFormula_NVD = new System.Windows.Forms.PictureBox();
             this.groupBoxNamedTwo_NVD.SuspendLayout();
             this.groupBoxNamedThree_NVD.SuspendLayout();
             this.groupBoxNamedFour_NVD.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFormula_NVD)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pictureBoxNamedTwo_NVD
-            // 
-            this.pictureBoxNamedTwo_NVD.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxNamedTwo_NVD.Image")));
-            this.pictureBoxNamedTwo_NVD.Location = new System.Drawing.Point(299, 12);
-            this.pictureBoxNamedTwo_NVD.Name = "pictureBoxNamedTwo_NVD";
-            this.pictureBoxNamedTwo_NVD.Size = new System.Drawing.Size(361, 52);
-            this.pictureBoxNamedTwo_NVD.TabIndex = 0;
-            this.pictureBoxNamedTwo_NVD.TabStop = false;
             // 
             // groupBoxNamedTwo_NVD
             // 
+            this.groupBoxNamedTwo_NVD.Controls.Add(this.pictureBoxFormula_NVD);
             this.groupBoxNamedTwo_NVD.Controls.Add(this.textBoxNamedFour_NVD);
-            this.groupBoxNamedTwo_NVD.Controls.Add(this.pictureBoxNamedTwo_NVD);
             this.groupBoxNamedTwo_NVD.Location = new System.Drawing.Point(12, 25);
             this.groupBoxNamedTwo_NVD.Name = "groupBoxNamedTwo_NVD";
             this.groupBoxNamedTwo_NVD.Size = new System.Drawing.Size(666, 184);
@@ -163,6 +153,15 @@ namespace Tyuiu.NosovaVD.Sprint6.Task0.V21
             this.buttonHelp_NVD.UseVisualStyleBackColor = false;
             this.buttonHelp_NVD.Click += new System.EventHandler(this.buttonHelp_NVD_Click);
             // 
+            // pictureBoxFormula_NVD
+            // 
+            this.pictureBoxFormula_NVD.Image = global::Tyuiu.NosovaVD.Sprint6.Task0.V21.Properties.Resources.Формула;
+            this.pictureBoxFormula_NVD.Location = new System.Drawing.Point(289, 21);
+            this.pictureBoxFormula_NVD.Name = "pictureBoxFormula_NVD";
+            this.pictureBoxFormula_NVD.Size = new System.Drawing.Size(371, 55);
+            this.pictureBoxFormula_NVD.TabIndex = 2;
+            this.pictureBoxFormula_NVD.TabStop = false;
+            // 
             // FormMain
             // 
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
@@ -178,19 +177,18 @@ namespace Tyuiu.NosovaVD.Sprint6.Task0.V21
             this.Name = "FormMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Спринт 6 | Таск 0 | Вариант 21 | Носова В.Д.";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxNamedTwo_NVD)).EndInit();
             this.groupBoxNamedTwo_NVD.ResumeLayout(false);
             this.groupBoxNamedTwo_NVD.PerformLayout();
             this.groupBoxNamedThree_NVD.ResumeLayout(false);
             this.groupBoxNamedThree_NVD.PerformLayout();
             this.groupBoxNamedFour_NVD.ResumeLayout(false);
             this.groupBoxNamedFour_NVD.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFormula_NVD)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-        private System.Windows.Forms.PictureBox pictureBoxNamedTwo_NVD;
         private System.Windows.Forms.GroupBox groupBoxNamedTwo_NVD;
         private System.Windows.Forms.GroupBox groupBoxNamedThree_NVD;
         private System.Windows.Forms.GroupBox groupBoxNamedFour_NVD;
@@ -201,6 +199,7 @@ namespace Tyuiu.NosovaVD.Sprint6.Task0.V21
         private System.Windows.Forms.TextBox textBoxNamedFour_NVD;
         private System.Windows.Forms.GroupBox groupBoxNameddTwo_NVD;
         private System.Windows.Forms.TextBox textBoxResult_NVD;
+        private System.Windows.Forms.PictureBox pictureBoxFormula_NVD;
     }
 }
 
