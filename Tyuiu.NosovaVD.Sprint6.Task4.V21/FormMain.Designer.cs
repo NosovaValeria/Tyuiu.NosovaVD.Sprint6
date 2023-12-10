abb6a9a6e3054d38e169e94aa93d00d72c92bd54
NosovaValeria <Tyuiu.NosovaVD@outlook.com>
2023-12-10 22:23:37 +0500

    Task4

--- Tyuiu.NosovaVD.Sprint6.Task4.V21/FormMain.Designer.cs
+++ Tyuiu.NosovaVD.Sprint6.Task4.V21/FormMain.Designer.cs
@@ -34,64 +34,64 @@ namespace Tyuiu.NosovaVD.Sprint6.Task4.V21
             System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
-            this.groupBoxNameddTwo_NVD = new System.Windows.Forms.GroupBox();
-            this.textBoxNameedOne_NVD = new System.Windows.Forms.TextBox();
-            this.textBoxNamedThree_NVD = new System.Windows.Forms.TextBox();
+            this.groupBoxIn_NVD = new System.Windows.Forms.GroupBox();
+            this.textBoxEnd_NVD = new System.Windows.Forms.TextBox();
+            this.textBoxStart_NVD = new System.Windows.Forms.TextBox();
             this.textBoxStopStep_NVD = new System.Windows.Forms.TextBox();
             this.textBoxStartStep_NVD = new System.Windows.Forms.TextBox();
-            this.groupBoxNameedThree_NVD = new System.Windows.Forms.GroupBox();
+            this.groupBoxOut_NVD = new System.Windows.Forms.GroupBox();
             this.textBoxResult_NVD = new System.Windows.Forms.TextBox();
             this.buttonHelp_NVD = new System.Windows.Forms.Button();
             this.buttonDone_NVD = new System.Windows.Forms.Button();
-            this.groupBoxNameedOne_NVD = new System.Windows.Forms.GroupBox();
-            this.textBoxNaamedOne_NVD = new System.Windows.Forms.TextBox();
+            this.groupBoxUslovie_NVD = new System.Windows.Forms.GroupBox();
+            this.textBoxUslovie_NVD = new System.Windows.Forms.TextBox();
             this.buttonSave_NVD = new System.Windows.Forms.Button();
             this.chartFunction_NVD = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.panelOne_NVD = new System.Windows.Forms.Panel();
-            this.panelTwo_NVD = new System.Windows.Forms.Panel();
+            this.panelTop_NVD = new System.Windows.Forms.Panel();
+            this.panelLeft_NVD = new System.Windows.Forms.Panel();
             this.splitterOne_NVD = new System.Windows.Forms.Splitter();
-            this.panelThree_NVD = new System.Windows.Forms.Panel();
-            this.groupBoxNameddTwo_NVD.SuspendLayout();
-            this.groupBoxNameedThree_NVD.SuspendLayout();
-            this.groupBoxNameedOne_NVD.SuspendLayout();
+            this.panelRight_NVD = new System.Windows.Forms.Panel();
+            this.groupBoxIn_NVD.SuspendLayout();
+            this.groupBoxOut_NVD.SuspendLayout();
+            this.groupBoxUslovie_NVD.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.chartFunction_NVD)).BeginInit();
-            this.panelOne_NVD.SuspendLayout();
-            this.panelTwo_NVD.SuspendLayout();
-            this.panelThree_NVD.SuspendLayout();
+            this.panelTop_NVD.SuspendLayout();
+            this.panelLeft_NVD.SuspendLayout();
+            this.panelRight_NVD.SuspendLayout();
             this.SuspendLayout();
             // 
-            // groupBoxNameddTwo_NVD
+            // groupBoxIn_NVD
             // 
-            this.groupBoxNameddTwo_NVD.Controls.Add(this.textBoxNameedOne_NVD);
-            this.groupBoxNameddTwo_NVD.Controls.Add(this.textBoxNamedThree_NVD);
-            this.groupBoxNameddTwo_NVD.Controls.Add(this.textBoxStopStep_NVD);
-            this.groupBoxNameddTwo_NVD.Controls.Add(this.textBoxStartStep_NVD);
-            this.groupBoxNameddTwo_NVD.Location = new System.Drawing.Point(525, 3);
-            this.groupBoxNameddTwo_NVD.Name = "groupBoxNameddTwo_NVD";
-            this.groupBoxNameddTwo_NVD.Size = new System.Drawing.Size(265, 82);
-            this.groupBoxNameddTwo_NVD.TabIndex = 12;
-            this.groupBoxNameddTwo_NVD.TabStop = false;
-            this.groupBoxNameddTwo_NVD.Text = "Ввод данных:";
+            this.groupBoxIn_NVD.Controls.Add(this.textBoxEnd_NVD);
+            this.groupBoxIn_NVD.Controls.Add(this.textBoxStart_NVD);
+            this.groupBoxIn_NVD.Controls.Add(this.textBoxStopStep_NVD);
+            this.groupBoxIn_NVD.Controls.Add(this.textBoxStartStep_NVD);
+            this.groupBoxIn_NVD.Location = new System.Drawing.Point(525, 3);
+            this.groupBoxIn_NVD.Name = "groupBoxIn_NVD";
+            this.groupBoxIn_NVD.Size = new System.Drawing.Size(265, 82);
+            this.groupBoxIn_NVD.TabIndex = 12;
+            this.groupBoxIn_NVD.TabStop = false;
+            this.groupBoxIn_NVD.Text = "Ввод данных:";
             // 
-            // textBoxNameedOne_NVD
+            // textBoxEnd_NVD
             // 
-            this.textBoxNameedOne_NVD.BackColor = System.Drawing.SystemColors.Control;
-            this.textBoxNameedOne_NVD.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBoxNameedOne_NVD.Location = new System.Drawing.Point(133, 21);
-            this.textBoxNameedOne_NVD.Name = "textBoxNameedOne_NVD";
-            this.textBoxNameedOne_NVD.Size = new System.Drawing.Size(114, 15);
-            this.textBoxNameedOne_NVD.TabIndex = 6;
-            this.textBoxNameedOne_NVD.Text = "Конец шага:";
+            this.textBoxEnd_NVD.BackColor = System.Drawing.SystemColors.Control;
+            this.textBoxEnd_NVD.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxEnd_NVD.Location = new System.Drawing.Point(133, 21);
+            this.textBoxEnd_NVD.Name = "textBoxEnd_NVD";
+            this.textBoxEnd_NVD.Size = new System.Drawing.Size(114, 15);
+            this.textBoxEnd_NVD.TabIndex = 6;
+            this.textBoxEnd_NVD.Text = "Конец шага:";
             // 
-            // textBoxNamedThree_NVD
+            // textBoxStart_NVD
             // 
-            this.textBoxNamedThree_NVD.BackColor = System.Drawing.SystemColors.Control;
-            this.textBoxNamedThree_NVD.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBoxNamedThree_NVD.Location = new System.Drawing.Point(13, 21);
-            this.textBoxNamedThree_NVD.Name = "textBoxNamedThree_NVD";
-            this.textBoxNamedThree_NVD.Size = new System.Drawing.Size(85, 15);
-            this.textBoxNamedThree_NVD.TabIndex = 5;
-            this.textBoxNamedThree_NVD.Text = "Старт шага:";
+            this.textBoxStart_NVD.BackColor = System.Drawing.SystemColors.Control;
+            this.textBoxStart_NVD.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxStart_NVD.Location = new System.Drawing.Point(13, 21);
+            this.textBoxStart_NVD.Name = "textBoxStart_NVD";
+            this.textBoxStart_NVD.Size = new System.Drawing.Size(85, 15);
+            this.textBoxStart_NVD.TabIndex = 5;
+            this.textBoxStart_NVD.Text = "Старт шага:";
             // 
             // textBoxStopStep_NVD
             // 
@@ -111,16 +111,16 @@ namespace Tyuiu.NosovaVD.Sprint6.Task4.V21
             this.textBoxStartStep_NVD.TabIndex = 5;
             this.textBoxStartStep_NVD.Text = "-5";
             // 
-            // groupBoxNameedThree_NVD
+            // groupBoxOut_NVD
             // 
-            this.groupBoxNameedThree_NVD.Controls.Add(this.textBoxResult_NVD);
-            this.groupBoxNameedThree_NVD.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBoxNameedThree_NVD.Location = new System.Drawing.Point(0, 0);
-            this.groupBoxNameedThree_NVD.Name = "groupBoxNameedThree_NVD";
-            this.groupBoxNameedThree_NVD.Size = new System.Drawing.Size(273, 350);
-            this.groupBoxNameedThree_NVD.TabIndex = 0;
-            this.groupBoxNameedThree_NVD.TabStop = false;
-            this.groupBoxNameedThree_NVD.Text = "Вывод";
+            this.groupBoxOut_NVD.Controls.Add(this.textBoxResult_NVD);
+            this.groupBoxOut_NVD.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBoxOut_NVD.Location = new System.Drawing.Point(0, 0);
+            this.groupBoxOut_NVD.Name = "groupBoxOut_NVD";
+            this.groupBoxOut_NVD.Size = new System.Drawing.Size(273, 350);
+            this.groupBoxOut_NVD.TabIndex = 0;
+            this.groupBoxOut_NVD.TabStop = false;
+            this.groupBoxOut_NVD.Text = "Вывод";
             // 
             // textBoxResult_NVD
             // 
@@ -156,26 +156,26 @@ namespace Tyuiu.NosovaVD.Sprint6.Task4.V21
             this.buttonDone_NVD.UseVisualStyleBackColor = false;
             this.buttonDone_NVD.Click += new System.EventHandler(this.buttonDone_NVD_Click);
             // 
-            // groupBoxNameedOne_NVD
+            // groupBoxUslovie_NVD
             // 
-            this.groupBoxNameedOne_NVD.Controls.Add(this.textBoxNaamedOne_NVD);
-            this.groupBoxNameedOne_NVD.Location = new System.Drawing.Point(3, 3);
-            this.groupBoxNameedOne_NVD.Name = "groupBoxNameedOne_NVD";
-            this.groupBoxNameedOne_NVD.Size = new System.Drawing.Size(516, 91);
-            this.groupBoxNameedOne_NVD.TabIndex = 8;
-            this.groupBoxNameedOne_NVD.TabStop = false;
-            this.groupBoxNameedOne_NVD.Text = "Условие";
+            this.groupBoxUslovie_NVD.Controls.Add(this.textBoxUslovie_NVD);
+            this.groupBoxUslovie_NVD.Location = new System.Drawing.Point(3, 3);
+            this.groupBoxUslovie_NVD.Name = "groupBoxUslovie_NVD";
+            this.groupBoxUslovie_NVD.Size = new System.Drawing.Size(516, 91);
+            this.groupBoxUslovie_NVD.TabIndex = 8;
+            this.groupBoxUslovie_NVD.TabStop = false;
+            this.groupBoxUslovie_NVD.Text = "Условие";
             // 
-            // textBoxNaamedOne_NVD
+            // textBoxUslovie_NVD
             // 
-            this.textBoxNaamedOne_NVD.BackColor = System.Drawing.SystemColors.Control;
-            this.textBoxNaamedOne_NVD.Location = new System.Drawing.Point(6, 21);
-            this.textBoxNaamedOne_NVD.Multiline = true;
-            this.textBoxNaamedOne_NVD.Name = "textBoxNaamedOne_NVD";
-            this.textBoxNaamedOne_NVD.ReadOnly = true;
-            this.textBoxNaamedOne_NVD.Size = new System.Drawing.Size(504, 61);
-            this.textBoxNaamedOne_NVD.TabIndex = 3;
-            this.textBoxNaamedOne_NVD.Text = resources.GetString("textBoxNaamedOne_NVD.Text");
+            this.textBoxUslovie_NVD.BackColor = System.Drawing.SystemColors.Control;
+            this.textBoxUslovie_NVD.Location = new System.Drawing.Point(6, 21);
+            this.textBoxUslovie_NVD.Multiline = true;
+            this.textBoxUslovie_NVD.Name = "textBoxUslovie_NVD";
+            this.textBoxUslovie_NVD.ReadOnly = true;
+            this.textBoxUslovie_NVD.Size = new System.Drawing.Size(504, 61);
+            this.textBoxUslovie_NVD.TabIndex = 3;
+            this.textBoxUslovie_NVD.Text = resources.GetString("textBoxUslovie_NVD.Text");
             // 
             // buttonSave_NVD
             // 
@@ -212,27 +212,27 @@ namespace Tyuiu.NosovaVD.Sprint6.Task4.V21
             title1.Text = "График функции";
             this.chartFunction_NVD.Titles.Add(title1);
             // 
-            // panelOne_NVD
+            // panelTop_NVD
             // 
-            this.panelOne_NVD.Controls.Add(this.groupBoxNameedOne_NVD);
-            this.panelOne_NVD.Controls.Add(this.buttonDone_NVD);
-            this.panelOne_NVD.Controls.Add(this.buttonSave_NVD);
-            this.panelOne_NVD.Controls.Add(this.buttonHelp_NVD);
-            this.panelOne_NVD.Controls.Add(this.groupBoxNameddTwo_NVD);
-            this.panelOne_NVD.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelOne_NVD.Location = new System.Drawing.Point(0, 0);
-            this.panelOne_NVD.Name = "panelOne_NVD";
-            this.panelOne_NVD.Size = new System.Drawing.Size(1132, 100);
-            this.panelOne_NVD.TabIndex = 7;
+            this.panelTop_NVD.Controls.Add(this.groupBoxUslovie_NVD);
+            this.panelTop_NVD.Controls.Add(this.buttonDone_NVD);
+            this.panelTop_NVD.Controls.Add(this.buttonSave_NVD);
+            this.panelTop_NVD.Controls.Add(this.buttonHelp_NVD);
+            this.panelTop_NVD.Controls.Add(this.groupBoxIn_NVD);
+            this.panelTop_NVD.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTop_NVD.Location = new System.Drawing.Point(0, 0);
+            this.panelTop_NVD.Name = "panelTop_NVD";
+            this.panelTop_NVD.Size = new System.Drawing.Size(1132, 100);
+            this.panelTop_NVD.TabIndex = 7;
             // 
-            // panelTwo_NVD
+            // panelLeft_NVD
             // 
-            this.panelTwo_NVD.Controls.Add(this.groupBoxNameedThree_NVD);
-            this.panelTwo_NVD.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panelTwo_NVD.Location = new System.Drawing.Point(0, 100);
-            this.panelTwo_NVD.Name = "panelTwo_NVD";
-            this.panelTwo_NVD.Size = new System.Drawing.Size(273, 350);
-            this.panelTwo_NVD.TabIndex = 8;
+            this.panelLeft_NVD.Controls.Add(this.groupBoxOut_NVD);
+            this.panelLeft_NVD.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelLeft_NVD.Location = new System.Drawing.Point(0, 100);
+            this.panelLeft_NVD.Name = "panelLeft_NVD";
+            this.panelLeft_NVD.Size = new System.Drawing.Size(273, 350);
+            this.panelLeft_NVD.TabIndex = 8;
             // 
             // splitterOne_NVD
             // 
@@ -242,61 +242,61 @@ namespace Tyuiu.NosovaVD.Sprint6.Task4.V21
             this.splitterOne_NVD.TabIndex = 17;
             this.splitterOne_NVD.TabStop = false;
             // 
-            // panelThree_NVD
+            // panelRight_NVD
             // 
-            this.panelThree_NVD.Controls.Add(this.chartFunction_NVD);
-            this.panelThree_NVD.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelThree_NVD.Location = new System.Drawing.Point(276, 100);
-            this.panelThree_NVD.Name = "panelThree_NVD";
-            this.panelThree_NVD.Size = new System.Drawing.Size(856, 350);
-            this.panelThree_NVD.TabIndex = 10;
+            this.panelRight_NVD.Controls.Add(this.chartFunction_NVD);
+            this.panelRight_NVD.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelRight_NVD.Location = new System.Drawing.Point(276, 100);
+            this.panelRight_NVD.Name = "panelRight_NVD";
+            this.panelRight_NVD.Size = new System.Drawing.Size(856, 350);
+            this.panelRight_NVD.TabIndex = 10;
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1132, 450);
-            this.Controls.Add(this.panelThree_NVD);
+            this.Controls.Add(this.panelRight_NVD);
             this.Controls.Add(this.splitterOne_NVD);
-            this.Controls.Add(this.panelTwo_NVD);
-            this.Controls.Add(this.panelOne_NVD);
+            this.Controls.Add(this.panelLeft_NVD);
+            this.Controls.Add(this.panelTop_NVD);
             this.MinimumSize = new System.Drawing.Size(1150, 497);
             this.Name = "FormMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Спринт 6 | Таск 4 | Вариант 21 | Носова В.Д.";
-            this.groupBoxNameddTwo_NVD.ResumeLayout(false);
-            this.groupBoxNameddTwo_NVD.PerformLayout();
-            this.groupBoxNameedThree_NVD.ResumeLayout(false);
-            this.groupBoxNameedThree_NVD.PerformLayout();
-            this.groupBoxNameedOne_NVD.ResumeLayout(false);
-            this.groupBoxNameedOne_NVD.PerformLayout();
+            this.groupBoxIn_NVD.ResumeLayout(false);
+            this.groupBoxIn_NVD.PerformLayout();
+            this.groupBoxOut_NVD.ResumeLayout(false);
+            this.groupBoxOut_NVD.PerformLayout();
+            this.groupBoxUslovie_NVD.ResumeLayout(false);
+            this.groupBoxUslovie_NVD.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.chartFunction_NVD)).EndInit();
-            this.panelOne_NVD.ResumeLayout(false);
-            this.panelTwo_NVD.ResumeLayout(false);
-            this.panelThree_NVD.ResumeLayout(false);
+            this.panelTop_NVD.ResumeLayout(false);
+            this.panelLeft_NVD.ResumeLayout(false);
+            this.panelRight_NVD.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.GroupBox groupBoxNameddTwo_NVD;
-        private System.Windows.Forms.TextBox textBoxNameedOne_NVD;
-        private System.Windows.Forms.TextBox textBoxNamedThree_NVD;
+        private System.Windows.Forms.GroupBox groupBoxIn_NVD;
+        private System.Windows.Forms.TextBox textBoxEnd_NVD;
+        private System.Windows.Forms.TextBox textBoxStart_NVD;
         private System.Windows.Forms.TextBox textBoxStopStep_NVD;
         private System.Windows.Forms.TextBox textBoxStartStep_NVD;
-        private System.Windows.Forms.GroupBox groupBoxNameedThree_NVD;
+        private System.Windows.Forms.GroupBox groupBoxOut_NVD;
         private System.Windows.Forms.Button buttonHelp_NVD;
         private System.Windows.Forms.Button buttonDone_NVD;
-        private System.Windows.Forms.GroupBox groupBoxNameedOne_NVD;
-        private System.Windows.Forms.TextBox textBoxNaamedOne_NVD;
+        private System.Windows.Forms.GroupBox groupBoxUslovie_NVD;
+        private System.Windows.Forms.TextBox textBoxUslovie_NVD;
         private System.Windows.Forms.Button buttonSave_NVD;
         private System.Windows.Forms.TextBox textBoxResult_NVD;
         private System.Windows.Forms.DataVisualization.Charting.Chart chartFunction_NVD;
-        private System.Windows.Forms.Panel panelOne_NVD;
-        private System.Windows.Forms.Panel panelTwo_NVD;
+        private System.Windows.Forms.Panel panelTop_NVD;
+        private System.Windows.Forms.Panel panelLeft_NVD;
         private System.Windows.Forms.Splitter splitterOne_NVD;
-        private System.Windows.Forms.Panel panelThree_NVD;
+        private System.Windows.Forms.Panel panelRight_NVD;
     }
 }
 
